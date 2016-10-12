942e456789d2ce23c81b8cea70652a65bc653596
Martin <martinbutts@clear.net.nz>
2016-10-13 11:56:16 +1300

    routes set up for speakers

--- app/app.js
+++ app/app.js
@@ -2,10 +2,16 @@ var express = require('express');
 var app = express();
 var dataFile = require('./data/data.json');
 
-app.set('port', process.env.PORT || 3000);
-
+app.set('port', process.env.PORT || 3000 );
 
 app.get('/', function(req, res) {
+  res.send(`
+      <h1>Welcome</h1>
+      <p>Hutt Artists Meetups put together artists from all walks of life</p>
+  `);
+});
+
+app.get('/speakers', function(req, res) {
   var info = '';
   dataFile.speakers.forEach(function(item) {
     info += `
@@ -14,14 +20,23 @@ app.get('/', function(req, res) {
       <p>${item.summary}</p>
     </li>
     `;
+  });
+  res.send(`
+      <h1>Hutt Artist Meetups</h1>
+      ${info}
+  `);
 });
 
+app.get('/speakers/:speakerid', function(req, res) {
+
+  var speaker = dataFile.speakers[req.params.speakerid];
   res.send(`
-    <h1>Hutt Valley Devs</h1>
-    ${info}
+      <h1>${speaker.title}</h1>
+      <h2>with ${speaker.name}</h2>
+      <p>${speaker.summary}</p>
   `);
 });
 
 var server = app.listen(app.get('port'), function() {
-  console.log('Listening on port' + app.get('port'));
+  console.log('Listening on port ' + app.get('port'));
 });
